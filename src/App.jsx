7861f21db4d9e40d0c135a8f6c8ebe61abbcca6c
Callum Govender <callumlevigovender@gmail.com>
2024-11-15 12:07:29 +0200

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Header from "./components/header.jsx";
+import Meme from "./components/meme.jsx";
 
 function App() {
-
   return (
     <>
-     
+      <Header></Header>
+      <Meme></Meme>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
